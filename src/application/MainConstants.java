176c5bb374e8a148a675fa86e7403d7251fa1246
well <wellmend0@gmail.com>
2020-09-12 22:49:31 -0300

    reduce height

--- src/application/MainConstants.java
+++ src/application/MainConstants.java
@@ -3,5 +3,5 @@ package application;
 public class MainConstants {
 	public static String TITLE_TEXT = "UFT";
 	public static Integer SCENE_WIDTH = 260;
-	public static Integer SCENE_HEIGHT = 550;
+	public static Integer SCENE_HEIGHT = 360;
 }
